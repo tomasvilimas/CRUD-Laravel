81ec3ab1133c50cb3f660480e4dbcc68b481fcc5
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2021-04-19 17:46:46 +0300

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(BlogpostSeeder::class);
+       
         $this->call(UserSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,9 +16,9 @@ class UserSeeder extends Seeder
     public function run()
     {
         $user1 = new User();
-        $user1->name = 'Tomasas';
-        $user1->email = 'admin@a.lt';
-        $user1->password = Hash::make('aaaaaaaa');
+        $user1->name = 'Admin';
+        $user1->email = 'admin@studentas.lt';
+        $user1->password = Hash::make('Neatspejamas');
         $user1->save();
     }
 }
